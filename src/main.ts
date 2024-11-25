fcd0fe533143009c88ea96f79b32c334d430bac3
Lamia Rizk <lamiarizk@gmx.de>
2024-11-25 13:53:23 +0100

    fetchURL

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
 import './style.css'
-import { IQuestion, IQuizData } from './Interfaces/IQuiz';
+import IQuestion from './Interfaces/IQuiz';
+import IQuizData from './Interfaces/IQuiz';
+
+const BASE_URL = "https://vz-wd-24-01.github.io/typescript-quiz/questions"
+const LANGUAGES = ['de', 'en'];
+const DIFFICULTY = ['easy', 'hard'];
+
+
+function buildQuizURL(language: typeof LANGUAGES[number], difficulty: typeof DIFFICULTY[number]): string {
+  if (language === 'de') {
+      difficulty = difficulty === 'easy' ? 'leicht' : 'schwer';
+  }
+  return `${BASE_URL}/${difficulty}.json`;
+}
+console.log(buildQuizURL('en', 'easy'));
+console.log(buildQuizURL('de', 'hard'));
+
+
+
 
 
 
